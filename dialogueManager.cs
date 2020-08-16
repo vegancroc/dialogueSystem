2abae75c3fdbed04edc1279da9f7f7fd14b1ec06
croc <69516460+vegancroc@users.noreply.github.com>
2020-08-16 17:28:48 +0300

    Add files via upload

---
+++ dialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text DialogueText;
+    public Animator animator;
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(dialogue dialogue)
+    {
+        helper.helperNumber = 0;
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        DialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            DialogueText.text += letter;
+            yield return new WaitForSeconds(0.1F);
+        }
+    }
+
+    void EndDialogue()
+    {
+        helper.helperNumber = 1;
+        animator.SetBool("IsOpen", false);
+    }
+
+}
---
+++ interaction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+    public class interaction : MonoBehaviour
+    {
+    public dialogue dialogue;
+        public LayerMask myLayer;
+        public Transform myObj;
+        [SerializeField] private float radius;
+        private bool playerDetector;
+
+        void Update()
+        {
+            playerDetector = Physics2D.OverlapCircle(myObj.position, radius, myLayer);
+            if (playerDetector)
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                FindObjectOfType<dialogueManager>().StartDialogue(dialogue);
+                }
+                
+            }
+        }
+    }
+
+
